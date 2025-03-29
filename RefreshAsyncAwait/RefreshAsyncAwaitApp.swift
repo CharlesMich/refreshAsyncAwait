c8840aecf441b32ff1537cdda47e8af56dd1123b
CharlesMich <charleedongo@hotmail.com>
2025-03-29 11:38:45 -0500

    Initial Commit

---
+++ RefreshAsyncAwait/RefreshAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefreshAsyncAwaitApp.swift
+//  RefreshAsyncAwait
+//
+//  Created by Charles Michael on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct RefreshAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
